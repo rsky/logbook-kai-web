7bc81f0a8b0be1f952fcf1e5caff6fa5883d59ef
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2019-09-13 16:13:13 +0900

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function (api) {
+    api.cache(true)
+
+    const presets = [
+        '@babel/env',
+        '@babel/react',
+        '@babel/typescript'
+    ]
+
+    const plugins = [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread'
+    ]
+
+    return { presets, plugins }
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/logbook',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            // Include ts, tsx, js, and jsx files.
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Logbook'
+        })
+    ],
+}
