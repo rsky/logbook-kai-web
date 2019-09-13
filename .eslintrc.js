11f1c92b838faed131f7e6c36b789736095b563d
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2019-09-13 17:12:45 +0900

    Introduce eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "standard",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+  ],
+  rules: {
+    "comma-dangle": ["error", "only-multiline"],
+    "eol-last": ["error", "unix"],
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+  }
+}
--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,14 @@ module.exports = function (api) {
     api.cache(true)
 
     const presets = [
-        '@babel/env',
-        '@babel/react',
-        '@babel/typescript'
+        "@babel/env",
+        "@babel/react",
+        "@babel/typescript",
     ]
 
     const plugins = [
-        '@babel/proposal-class-properties',
-        '@babel/proposal-object-rest-spread'
+        "@babel/proposal-class-properties",
+        "@babel/proposal-object-rest-spread",
     ]
 
     return { presets, plugins }
